76632fef20d52ffc9e4d8dbe7ebf082706aef06d
fedoseeva anna <anna.fedoseeva@kaspersky.com>
2025-11-13 15:09:24 +0300

    Интерфейс

--- Program.cs
+++ Program.cs
@@ -1,78 +1,58 @@
 ﻿using System;
 
-namespace Delegates
+namespace Interface
 {
-    public delegate void Sort(int number); //делегат
-
-    public class InvalidInputException : Exception
+    public interface ICalculate
     {
-        public InvalidInputException(string message) : base(message) { }
+        int Add(int num1, int num2);
     }
 
-    public class LastName //издатель
+    public class Calculate : ICalculate
     {
-        private List<string> _lastNames = new List<string>
+        public int Add(int num1, int num2)
         {
-            "Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов"
-        };
-
-        public event Sort TakeNumber; //событие
-        
-        public void SortAscending()
-        {
-            _lastNames.Sort();
-        }
-
-        public void SortDescending()
-        {
-            _lastNames.Sort(); 
-            _lastNames.Reverse();
-        }
-
-        public void OnSort(int number)
-        {
-            if (number == 1)
-            {
-                SortAscending();
-            }
-            else if (number == 2)
-            {
-                SortDescending();
-            }
-            else
-            {
-                throw new InvalidInputException("Можно вводить только цифры 1 или 2");
-            }
-        }
-
-        public void RaiseSortEvent(int number)
-        {
-            TakeNumber?.Invoke(number);
+            return num1 + num2;
         }
     }
 
-    public class Program
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;     
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;    
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+
+    public interface IWorker
+    {
+        void Work();
+    }
+
+    class Program
+
     {
         static void Main()
         {
-            LastName ln = new LastName();
-            ln.TakeNumber += ln.OnSort; //регистрируем событие
+            ILogger logger = new Logger();
+            ICalculate calculator = new Calculate();
+            IWorker worker = new Worker(logger, calculator);
 
-            try
-            {
-                Console.WriteLine("Введите 1, если хотите провести сортировку А-Я. Введите 2, если хотите провести сортировку Я-А");
-                int answer = Convert.ToInt32(Console.ReadLine());
-                ln.RaiseSortEvent(answer); //вызываем событие
-            }
-            catch (InvalidInputException ex)
-            {
-                Console.WriteLine("Ошибка: " + ex.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Сортировка завершена");
-            }
-
+            worker.Work();
         }
 
     }
---
+++ Worker.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Interface
+{
+    public class Worker : IWorker
+    {
+        ILogger Logger { get; }
+        ICalculate Calculator { get; }
+
+        public Worker(ILogger logger, ICalculate calculator)
+        {
+            Logger = logger;
+            Calculator = calculator;
+        }
+
+        public void Work()
+        {
+            try
+            {
+                Logger.Event("Worker начал работу калькулятора");
+
+                Console.Write("Введите первое число: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int result = Calculator.Add(a, b);
+                Logger.Event($"Результат сложения: {result}");
+            }
+            catch (FormatException)
+            {
+                Logger.Error("Ошибка: введено некорректное число!");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Неизвестная ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Logger.Event("Worker завершил работу калькулятора");
+            }
+        }
+    }
+}
